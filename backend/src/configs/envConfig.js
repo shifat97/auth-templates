346d7d735b2a0d619cd1e6c4d63b29048815f8b7
shifat97 <shifatbinreza2@gmail.com>
2025-09-20 13:45:39 +0600

    add: Env config files

---
+++ backend/src/configs/envConfig.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const envConfig = {
+  PORT: process.env.PORT || 5000,
+  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+  DB: {
+    MONGO_URI: process.env.MONGO_URI,
+    DB_NAME: process.env.DB_NAME,
+  },
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+};
+
+export default envConfig;
