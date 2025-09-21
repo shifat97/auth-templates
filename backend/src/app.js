99a8c34da2b07067f75392f8a0c8cbfdf8ef0b72
shifat97 <shifatbinreza2@gmail.com>
2025-09-21 20:07:37 +0600

    fix: Allowed origin port

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // frontend URL
+    origin: envConfig.ALLOWED_ORIGIN, // frontend URL
     credentials: true,
   })
 );
